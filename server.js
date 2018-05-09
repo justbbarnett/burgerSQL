af9c215b33eafceb16dc43183f7ced0afa3d0996
justbbarnett <justb@inkery.com>
2018-05-09 18:05:27 -0500

    server updated with reuire routes, routes created, env & gitnore added...

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var app = express();
 var PORT = process.env.PORT || 8080;
 
-var app = express();
+var db = require("./models")
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -21,9 +22,14 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgerController.js");
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
-app.use(routes);
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
