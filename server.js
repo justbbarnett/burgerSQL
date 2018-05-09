68d509260981bbe29fe4b9b01268051964b409db
justbbarnett <justb@inkery.com>
2018-05-09 18:23:54 -0500

    gitnore

--- server.js
+++ server.js
@@ -31,8 +31,4 @@ db.sequelize.sync({ force: true }).then(function() {
   });
 });
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+
